299040c2ad0b96d0ac2d3ba845ee89e781de759a
Vejmon <vemundhell@gmail.com>
2023-02-07 14:07:24 +0100

    vi er i gang

--- oblig1_2410/client.py
+++ oblig1_2410/client.py
@@ -1,19 +1,58 @@
+
+def gyldighet(listen, bokstaven):
+    if bokstaven in listen:
+        return True
+    else:
+        return False
+
+def game():
+    server_svar = clientSocket.recv(1024).decode()
+    while server_svar == "vent":
+        print("vi må vente på en motspiller")
+        server_svar = clientSocket.recv(1024).decode()
+    print("din motspiller er: " + server_svar)
+
+    gyldig = ["r","p","s"]
+    valg = input('velg r for rock, p for paper, eller s for scissor.').lower()
+    while not gyldighet(gyldig, valg):
+        print("prøv igjen")
+        valg = input('velg r for rock, p for paper, eller s for scissor.').lower()
+        gyldighet(gyldig, valg)
+    clientSocket.send(valg.encode())
+    print("venter på svar")
+    # server svarer med enten 'gratulerer', eller 'taper'
+    print(clientSocket.recv(1024).decode())
+
+#thread
 from socket import *
-def main():
-    message = "Hello world"
-    client_sd = socket(AF_INET,SOCK_STREAM)
-    server_ip = '127.0.0.1'
-    port = 12000
+import sys
+serverName = '127.0.0.1'
+serverPort = 12000
+clientSocket = socket(AF_INET, SOCK_STREAM)
+try:
+    clientSocket.connect((serverName,serverPort))
+except:
+    print("ConnectionError")
+    sys.exit()
+while True:
+    sentence = input('Welcome! Type "broadcast" if you want to broadcast a message or type "game" if you want to play rock, paper, scissor').lower()
+    if sentence == "broadcast":
+        #vil broadcaste noe
+        clientSocket.send(sentence.encode())
+    elif sentence == "game":
+        #vil spille spill
+        clientSocket.send(sentence.encode())
+        game()
+    elif sentence == "exit":
+        break
+    else:
+        print("Not valid input, try again")
 
-    #connect to the server
-    client_sd.connect((server_ip, port))
+    modifiedSentence = clientSocket.recv(1024)
+    print ('From Server:', modifiedSentence.decode())
 
-    #Send data
-    client_sd.send(message.encode());
 
-    #Read data from the socket
-    received_line = client_sd.recv(1024).decode()
 
-    #print
-    print(received_line)
-    client_sd.close()
+
+
+clientSocket.close()
--- oblig1_2410/server.py
+++ oblig1_2410/server.py
@@ -1,23 +1,92 @@
+"""
+Server side: open a socket on a port, listen for a message 
+from a client, and send an echo reply; 
+echoes lines until eof when client closes socket; spawns a 
+thread to handle each client connection; threads share global 
+memory space with main thread.
+"""
 from socket import *
+import _thread as thread
+import time
+
+class Client(thread.Thread):
+    def __init__(self,ip,port,connection):
+        thread.Thread.__init__(self)
+        self.connection=connection
+        self.ip=ip
+        self.port=port
+
+def now():
+    """
+    returns the time of day
+    """
+    return time.ctime(time.time())
+
+def handleClient(connection):
+    """
+    a client handler function
+    """
+    while True:
+        data = connection.recv(1024).decode() #receives input from server
+        print ("received  message = ", data)    #prints the received message
+        modified_message= data.lower()      #puts the message to lowercase
+        # connection.send(modified_message.encode())
+        if modified_message== "broadcast":
+            broadcast()
+        elif modified_message== "game":
+            game()
+        elif modified_message == "exit":
+            break
+    connection.close()
+
+
 def main():
-    server_sd = socket(AF_INET, SOCK_STREAM)
-    port = 12000
-    server_ip = '127.0.0.1'
+    """
+    creates a server socket, listens for new connections,
+    and spawns a new thread whenever a new connection join
+    """
+    serverPort = 12000
+    serverSocket = socket(AF_INET,SOCK_STREAM)
+    try:
+        serverSocket.bind(('',serverPort))
+    except:
+        print("Bind failed. Error : ")
+    serverSocket.listen(1)
+    print ('The server is ready to receive')
+    while True:
+        connectionSocket, addr = serverSocket.accept()
+        print('Server connected by ', addr)
+        print('at ', now())
+        thread.start_new_thread(handleClient, (connectionSocket,))
 
-    #bind
-    server_sd.bind((server_ip, port))
+    serverSocket.close()
 
-    #activate listening on the socket
-    server_sd.listen(1)
-    #server waits on accept for connection
-    conn_sd, addr = server_sd.accept()
+if __name__ == '__main__':
+    main()
 
-    #read data from the client and print
-    recieved_line = conn_sd.recv(1024).decode()
-    print(recieved_line)
+antall_spillere = 0
+def game(antall_spillere): #allow two clients to play rock,paper, scissors game
+    antall_spillere += 1
+    #lagre klient 1 og 2, få de til å sende stein, saks eller pair og si hvem som vinner
+    if antall_spillere == 1:
+        client1=thread.get_ident()
+        client1.send("vent må motspiller".encode())
+        time.sleep(1)
+    else:
+        antall_spillere = 0
 
-    #send data back
-    conn_sd.send(recieved_line.encode())
-    conn_sd.close()
-    server_sd.close()
+def broadcast(): #notify everyone when a client joins (except the client who joined)
 
+def messageall(connection):
+    msg = connection.recv(1024).decode()
+    for client in self.clients :
+        client.connection.send(msg)
+
+def sendmessagetoall():
+    for client in self.clients :
+            client.connection.send(msg)
+
+def sendtoclient():
+    for client in self.clients :
+        if client.ip == ip and client.port ==port :
+            client.connection.send(msg)
