65a54a4f48873ada9ac965a7464e705444f5799a
StineSzymanek <s362097@oslomet.no>
2023-02-16 11:16:18 +0100

    Bruker message.strip() for å få vekk mellomrom, nå funker exit

--- oblig1_2410/safiqulSkeleton/server.py
+++ oblig1_2410/safiqulSkeleton/server.py
@@ -1,16 +1,18 @@
 """
-Server side: it simultaneously handle multiple clients
+Server side: it simultaneously handles multiple clients
 and broadcast when a client new client joins or a client
 sends a message.
 """
+import socket
 from socket import *
 import _thread as thread
 import time
 import sys
 
 
-#this is too keep all the newly joined connections! 
+# this is to keep all the newly joined connections!
 all_client_connections = []
+
 
 def now():
 	"""
@@ -18,44 +20,36 @@ def now():
 	"""
 	return time.ctime(time.time())
 
+
 def handleClient(connection, addr):
 	"""
 	a client handler function 
 	"""
-	#this is where we broadcast everyone that a new client has joined
-	
-	### Write your code here ###
-	# append this this to the list for broadcast
-	all_client_connections.append(connection)
-	# create a message to inform all other clients
-	newJoin = f"{addr} has just joined"
-	for client in all_client_connections:
-		if client != connection:
-			broadcast(client, newJoin)
-	# that a new client has just joined.
+	# this is where we broadcast everyone that a new client has joined
 
-	### Your code ends here ###
+	connection.send("Welcome!".encode())		# Sends a welcome message to the client
+	all_client_connections.append(connection)		# Appends the new connection to the list off all clients
+	newJoin = f"{addr} has just joined"		# Message to inform others that a new client has joined
+	broadcast(connection, newJoin)		# Broadcast the join-message
 
 	while True:
-		message = connection.recv(2048).decode()
-		print (now() + " " +  str(addr) + "#  ", message)
-		if (message == "exit" or not message):
+		message = connection.recv(2048).decode()		# Recieve the message from client
+		print(now() + " " + str(addr) + "#  ", message)
+		if message.strip() == "exit" or not message:		# Close connection if client sends exit or there isn't a message
 			break
-		### Write your code here ###
 		else:
-		#broadcast this message to the others
-			for client in all_client_connections:
-				if client != connection:
-					broadcast(client, message)
-		### Your code ends here ###
+			# broadcast this message to the others
+			broadcast(connection, message)
 	connection.close()
 	all_client_connections.remove(connection)
 
+
 def broadcast(connection, message):
-	print ("Broadcasting")
-	### Write your code here ###
-	connection.send(message.encode())
-	### Your code ends here ###
+	print("Broadcasting")
+	for client in all_client_connections:		# Broadcast the message to everyone except the client who sent it
+		if client != connection:
+			client.send(message.encode())
+
 
 def main():
 	"""
@@ -63,27 +57,22 @@ def main():
 	and spawns a new thread whenever a new connection join
 	"""
 	serverPort = 12000
-	serverSocket = socket(AF_INET,SOCK_STREAM)
+	serverSocket = socket(AF_INET, SOCK_STREAM)
 	try:
-		# Use the bind function wisely!
-		### Write your code here ###
-		serverSocket.bind(('',serverPort))
-		### Your code ends here ###
-		
-	except: 
+		serverSocket.bind(('', serverPort))		# Binding
+	except:
 		print("Bind failed. Error : ")
 		sys.exit()
-	serverSocket.listen(10)
-	print ('The server is ready to receive')
+	serverSocket.listen(10)		# Listen for connections
+	print('The server is ready to receive')
 	while True:
-		### Write your code here ###
 		connectionSocket, addr = serverSocket.accept()   # accept a connection
-		### You code ends here ###
-		 
+
 		print('Server connected by ', addr) 
 		print('at ', now())
-		thread.start_new_thread(handleClient, (connectionSocket,addr)) 
+		thread.start_new_thread(handleClient, (connectionSocket,addr))		# Start a new thread for the connection
 	serverSocket.close()
+
 
 if __name__ == '__main__':
 	main()
